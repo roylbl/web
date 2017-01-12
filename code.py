bbb2e71a1be9904a2cf624c8b734fea9a1cd0b3e
royliu <royliu@taiyear.com>
2017-01-12 10:57:21 +0800

    电影网站第一次提交，indexx

---
+++ code.py
@@ -0,0 +1,26 @@
+import web
+
+urls = (
+    '/','index'
+)
+
+movies = [
+    {
+        "title": "Forrest Gump",
+        "year": 1994
+    },
+    {
+        "title": "Titanic",
+        "year": 1997
+    }
+]
+class index:
+    def GET(self):
+        page = ''
+        for m in movies:
+            page += '%s(%d)\n' % (m["title"], m["year"])
+        return page
+
+if __name__ == "__main__":
+    app = web.application(urls, globals())
+    app.run()
