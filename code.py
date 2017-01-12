d5dc47c9d67258d8b92c31216806c26ed73b182a
royliu <royliu@taiyear.com>
2017-01-12 17:58:25 +0800

    更多页面

--- code.py
+++ code.py
@@ -2,17 +2,24 @@
 import web
 
 urls = (
-    '/','index'
+    '/', 'index',
+    '/movie/(\d+)', 'movie',
 )
 
 render = web.template.render('templates/')
-db = web.database(dbn='sqlite', db='MovieSite.db')
+db = web.database(dbn='sqlite', db='MovieSite1.db')
 
 class index:
     def GET(self):
         movies = db.select('movie')
         return render.index(movies)
 
+
+class movie:
+    def GET(self, movie_id):
+        movie_id = int(movie_id)
+        movie = db.select('movie', where='id=$movie_id', vars=locals())[0]
+        return render.movie(movie)
 if __name__ == "__main__":
     app = web.application(urls, globals())
     app.run()
